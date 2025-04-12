e22a8c33e521e3c39f2f99632c632a5bfe775db8
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-04-12 13:21:08 +0200

    feat: added postgres monitoring

--- terraform/tools/metrics.tf
+++ terraform/tools/metrics.tf
@@ -275,6 +275,18 @@ module "stalwart_metrics" {
   depends_on = [kubernetes_namespace.metrics_ns]
 }
 
+module "postgres_metrics" {
+  source = "./metrics-np"
+
+  namespace  = var.everest_ns
+  port       = 9187
+  name       = "prometheus-postgres-exporter"
+  metrics_ns = var.metrics_ns
+  selector   = "app == 'prometheus-postgres-exporter'"
+
+  depends_on = [kubernetes_namespace.metrics_ns]
+}
+
 module "ingress_nginx_dashboard" {
   source = "./dashboard"
 
@@ -348,6 +360,17 @@ module "minio_dashboard" {
   depends_on = [kubernetes_namespace.metrics_ns]
 }
 
+module "postgres_dashboard" {
+  source = "./dashboard"
+
+  name      = "postgres"
+  namespace = var.metrics_ns
+  url       = ""
+  download  = false
+
+  depends_on = [kubernetes_namespace.metrics_ns]
+}
+
 resource "kubectl_manifest" "cert_manager_prometheus_config" {
   yaml_body = yamlencode({
     apiVersion = "monitoring.coreos.com/v1"
@@ -393,6 +416,24 @@ resource "kubectl_manifest" "minio_prometheus_config" {
       }
     }
     spec = yamldecode(file("${path.module}/mixin/minio.yaml"))
+  })
+
+  depends_on = [kubernetes_namespace.metrics_ns]
+}
+
+resource "kubectl_manifest" "postgres_prometheus_config" {
+  yaml_body = yamlencode({
+    apiVersion = "monitoring.coreos.com/v1"
+    kind       = "PrometheusRule"
+    metadata = {
+      name      = "postgres"
+      namespace = var.metrics_ns
+      labels = {
+        prometheus = "postgres"
+        role       = "alert-rules"
+      }
+    }
+    spec = yamldecode(file("${path.module}/mixin/postgres.yaml"))
   })
 
   depends_on = [kubernetes_namespace.metrics_ns]
@@ -453,4 +494,38 @@ spec:
   YAML
 
   depends_on = [kubernetes_namespace.metrics_ns]
+}
+
+resource "helm_release" "postgres_exporter" {
+  name       = "postgres-exporter"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus-postgres-exporter"
+  version    = "6.10.0"
+  namespace  = var.everest_ns
+
+  values = [templatefile("${path.module}/templates/postgres-exporter.values.tftpl", {
+    namespace = var.everest_ns
+  })]
+
+  depends_on = [helm_release.prometheus]
+}
+
+resource "kubectl_manifest" "postgres_exporter_secret" {
+  yaml_body = <<YAML
+apiVersion: external-secrets.io/v1beta1
+kind: ExternalSecret
+metadata:
+  name: postgres-exporter
+  namespace: ${var.everest_ns}
+spec:
+  refreshInterval: 15s
+  secretStoreRef:
+    name: ${var.cluster_secret_store}
+    kind: ClusterSecretStore
+  target:
+    name: postgres-exporter
+  dataFrom:
+  - extract:
+      key: apps/postgres-exporter
+  YAML
 }
