4549e3cf082034a521cf86a5b031e725a35a923e
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-05-01 20:37:13 +0200

    feat: switched argocd apps to helm charts

--- terraform/tools/proton.tf
+++ terraform/tools/proton.tf
@@ -21,11 +21,39 @@ metadata:
 spec:
   project: default
   source:
-    repoURL: https://github.com/Profiidev/server-config
-    path: apps/proton
-    targetRevision: 1ca61df70d4caa1b764793f6417dc84d6479fa64
+    repoURL: https://profiidev.github.io/server-config
+    chart: proton
+    targetRevision: "*"
+    helm:
+      releaseName: proton
+      valuesObject:
+        secret:
+          storeName: ${var.cluster_secret_store}
+        backend:
+          extraVolumes:
+            - name: cluster-ca-cert
+              secret:
+                defaultMode: 420
+                secretName: cluster-ca-cert
+          extraVolumeMounts:
+            - name: cluster-ca-cert
+              readOnly: true
+              subPath: ${var.ca_hash}
+              mountPath: /etc/ssl/certs/${var.ca_hash}.0
+          ingress:
+            className: ${var.ingress_class}
+            annotations:
+              nginx.ingress.kubernetes.io/auth-tls-secret: ${var.proton_ns}/${var.cloudflare_ca_cert_var}
+              nginx.ingress.kubernetes.io/auth-tls-verify-client: "on"
+              nginx.ingress.kubernetes.io/rewrite-target: "/$1"
+            tls:
+              - hosts:
+                  - profidev.io
+                  - "*.profi.dev"
+                secretName: ${var.cloudflare_cert_var}
   destination:
     server: https://kubernetes.default.svc
+    namespace: ${var.proton_ns}
   syncPolicy:
     automated:
       prune: true
