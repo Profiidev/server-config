5c14d0898c97abed0619a00b49319fb0669e9836
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-10-11 12:48:42 +0200

    fix: added metallb api egress

--- terraform/network/metallb.tf
+++ terraform/network/metallb.tf
@@ -43,3 +43,38 @@ spec:
 
   depends_on = [helm_release.metallb]
 }
+
+resource "kubectl_manifest" "metallb_k8s_api_egress" {
+  yaml_body = <<YAML
+apiVersion: crd.projectcalico.org/v1
+kind: NetworkPolicy
+metadata:
+  name: k8s-api-egress
+  namespace: ${var.lb_ns}
+spec:
+  order: 10
+  types:
+    - Egress
+  egress:
+    - action: Allow
+      protocol: TCP
+      destination:
+        nets:
+          - 194.164.200.60/32
+          - 172.17.0.0/16
+        ports:
+          - 6443
+          - 9001
+    - action: Allow
+      protocol: TCP
+      destination:
+        notNets:
+          - 10.0.0.0/8
+          - 172.16.0.0/12
+          - 192.168.0.0/16
+        ports:
+          - 443
+  YAML
+
+  depends_on = [kubernetes_namespace.lb_ns]
+}
