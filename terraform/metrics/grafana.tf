fe716596f08453309242ffdaaca3bcb66be03427
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-11-22 23:17:54 +0100

    refactor: moved dashboards to new config

--- terraform/metrics/grafana.tf
+++ terraform/metrics/grafana.tf
@@ -13,3 +13,79 @@ resource "helm_release" "grafana" {
     cloudflare_cert_var    = var.cloudflare_cert_var
   })]
 }
+
+module "dashboards" {
+  for_each = toset([
+    "ingress-nginx",
+    "ingress-nginx-request",
+    "cert-manager",
+    "external-secrets",
+    "vault", "longhorn",
+    "postgres",
+    "nats",
+    "nats-jetstream",
+    "coderd",
+    "coder-workspaces",
+    "coder-workspace-detail",
+    "crowdsec-details",
+    "crowdsec-insight",
+    "crowdsec-overview",
+    "pod-logs",
+    "tempo-block-builder",
+    "tempo-operational",
+    "tempo-reads",
+    "tempo-resources",
+    "tempo-rollout-progress",
+    "tempo-tenants",
+    "tempo-writes",
+    "alloy-controller",
+    "alloy-logs",
+    "alloy-otel",
+    "alloy-prom",
+    "alloy-resources",
+    "loki-chunks",
+    "loki-deletion",
+    "loki-logs",
+    "loki-operational",
+    "loki-reads-resources",
+    "loki-reads",
+    "loki-retention",
+    "loki-writes-resources",
+    "loki-writes",
+    "argo-cd-application",
+    "argo-cd-notifications",
+    "argo-cd-operational",
+  ])
+
+  source = "../modules/grafana-dashboard"
+
+  name      = each.key
+  namespace = var.metrics_ns
+}
+
+resource "kubectl_manifest" "alert_configs" {
+  for_each = toset([
+    "cert-manager",
+    "longhorn",
+    "postgres",
+    "pgbouncer",
+    "coder",
+    "tempo",
+    "alloy",
+    "argocd",
+  ])
+
+  yaml_body = yamlencode({
+    apiVersion = "monitoring.coreos.com/v1"
+    kind       = "PrometheusRule"
+    metadata = {
+      name      = each.key
+      namespace = var.metrics_ns
+      labels = {
+        prometheus = each.key
+        role       = "alert-rules"
+      }
+    }
+    spec = yamldecode(file("${path.module}/mixin/${each.key}.yaml"))
+  })
+}
