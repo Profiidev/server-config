55e79f0a0d33d1d75296d9ab022e23a31f613eda
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-08-26 10:03:04 +0200

    chore: removed garnix cache

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,11 +2,9 @@
   description = "Cluster node config";
   nixConfig = {
     extra-substituters = [
-      "https://cache.garnix.io"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
