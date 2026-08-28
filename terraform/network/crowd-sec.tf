dbd2dbb67205baa2bb4a396fe1c813aa50430c3b
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2026-08-28 10:11:30 +0200

    chore: updated crowdsec webui

--- terraform/network/crowd-sec.tf
+++ terraform/network/crowd-sec.tf
@@ -63,7 +63,7 @@ resource "helm_release" "crowdsec_web_ui" {
   repository = "https://zekker6.github.io/helm-charts"
   chart      = "crowdsec-web-ui"
   namespace  = var.crowdsec_ns
-  version    = "0.49.0"
+  version    = "0.50.0"
 
   values = [
     templatefile("${path.module}/templates/crowdsec-web-ui.values.tftpl", {
